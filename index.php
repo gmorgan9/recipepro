381ac22646bea627c661ba8dafdd6eb5bb9875fa
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-23 16:39:45 -0600

    update

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
     </h1>
     <div class="bottom-nav ">
         <nav>
-            <ul class="d-flex flex-row">
+            <ul class="d-flex flex-row space-around">
                 <li><a class="text-white" href=""><i class="bi bi-house-door-fill"></i></a></li>
                 <li><a href=""><i class="bi bi-plus-circle-fill"></i></a></li>
                 <li><a href=""><i class="bi bi-book-fill"></i></a></li>
