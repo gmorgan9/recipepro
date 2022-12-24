de6fb33fd74396511fd3d116b01d23c1e147ccca
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-24 07:58:28 -0600

    update

--- index.php
+++ index.php
@@ -29,8 +29,10 @@
         <nav class="mobile-bottom-nav">
             <ul class="list d-flex justify-content-between">
                 <li><a class="text-white" style="font-size: 20px;" href=""><i class="bi bi-house-door-fill"></i></a></li>
-                <li><a class="text-white" style="font-size: 20px;" href=""><i class="bi bi-plus-circle-fill"></i></a></li>
+                <li><a class="text-white" style="font-size: 20px;" href=""><i class="bi bi-list-check"></i></a></li>
+                <li><a class="text-white" style="font-size: 20px;" href=""><i class="bi bi-calendar-week"></i></a></li>
                 <li><a class="text-white" style="font-size: 20px;" href=""><i class="bi bi-book-fill"></i></a></li>
+                <li><a class="text-white" style="font-size: 20px;" href=""><i class="bi bi-gear-fill"></i></a></li>
             </ul>
         </nav>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
