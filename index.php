a6146fb3e5c0b6949c8f09b9622383f51a1b65ef
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-24 07:49:55 -0600

    update

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <!-- end bootstrap -->
 
     <!-- custom css styles -->
-        <link rel="stylesheet" href="assets/styles.css?v=1.13">
+        <link rel="stylesheet" href="assets/styles.css?v=1.14">
     <!-- end custom css styles -->
 
     <title>Fuel Up</title>
