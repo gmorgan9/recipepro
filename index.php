5bb2163fc0d933c93be81f9bde7b3fd8ed4f2392
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-24 07:46:20 -0600

    update

--- index.php
+++ index.php
@@ -26,7 +26,6 @@
     <h1>
         hello world
     </h1>
-    <div class="bottom-nav">
         <nav class="mobile-bottom-nav">
             <ul class="d-flex flex-row justify-content-between">
                 <li><a class="text-white" style="font-size: 20px;" href=""><i class="bi bi-house-door-fill"></i></a></li>
@@ -34,7 +33,6 @@
                 <li><a class="text-white" style="font-size: 20px;" href=""><i class="bi bi-book-fill"></i></a></li>
             </ul>
         </nav>
-    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
