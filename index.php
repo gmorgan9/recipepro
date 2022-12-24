40ea6e75f92fea16568d07c71ee10737b73442df
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-24 15:42:55 -0600

    update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable = no">
 
     <link rel="manifest" href="manifest.json">
@@ -25,7 +25,7 @@
     <title>Recipe Pro</title>
 </head>
 <body>
-    <h1 scroll="no" style="overflow:hidden;">
+    <h1>
         Home
     </h1>
     <nav class="mobile-bottom-nav">
