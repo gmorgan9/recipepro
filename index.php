b8471d9ba73ebea606ebbf83dda1b5521d14fef8
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-24 07:43:15 -0600

    update

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-bar-style" content="black-translucent">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,7 +17,7 @@
     <!-- end bootstrap -->
 
     <!-- custom css styles -->
-        <link rel="stylesheet" href="assets/styles.css?v=1.04">
+        <link rel="stylesheet" href="assets/styles.css?v=1.05">
     <!-- end custom css styles -->
 
     <title>Fuel Up</title>
