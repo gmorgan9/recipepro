a7bed7b9c2b2ad8a4f56f1bf77ee1c43eb44ea00
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-12-24 08:11:09 -0600

    update

--- cookbook.php
+++ cookbook.php
@@ -33,7 +33,7 @@
                 <li><a class="d-flex flex-column text-white align-items-center" style="text-decoration: none; font-size: 20px;" href="shopping.php"><i class="bi bi-list-check" style="margin-bottom: -5px;"></i><span style="font-size: 12px;">Shopping</span></a></li>
                 <li><a class="d-flex flex-column text-white align-items-center" style="text-decoration: none; font-size: 20px;" href="planner.php"><i class="bi bi-calendar-week" style="margin-bottom: -5px;"></i><span style="font-size: 12px;">Planner</span></a></li>
                 <li><a class="d-flex flex-column text-white align-items-center" style="text-decoration: none; font-size: 20px;" href="cookbook.php"><i class="bi bi-book-fill" style="margin-bottom: -5px;"></i><span style="font-size: 12px;">Cookbooks</span></a></li>
-                <li><a class="d-flex flex-column text-white align-items-center" style="text-decoration: none; font-size: 20px;" href="setting.php"><i class="bi bi-gear-fill" style="margin-bottom: -5px;"></i><span style="font-size: 12px;">Settings</span></a></li>
+                <li><a class="d-flex flex-column text-white align-items-center" style="text-decoration: none; font-size: 20px;" href="settings.php"><i class="bi bi-gear-fill" style="margin-bottom: -5px;"></i><span style="font-size: 12px;">Settings</span></a></li>
             </ul>
         </nav>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
